82f9fe4696006e29baaed20bd6aac4681ba632cf
kovchin <kovchin@mail.ru>
2020-08-26 17:23:39 +0700

    Настроил подключение к базе данных и подключил bootstrap

---
+++ app/Http/Controllers/Relations.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Relations extends Controller
+{
+    public function main()
+    {
+        return view('db/main');
+    }
+}
